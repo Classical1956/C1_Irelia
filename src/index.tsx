7700bddde32df6f73847be0164097f4639de794c
dracula1956 <546799674@qq.com>
2018-04-12 19:55:32 +0800

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-// import 'antd-mobile/dist/antd-mobile.min.css';
+import 'antd-mobile/dist/antd-mobile.min.css';
 import { createHashHistory } from 'history';
 import Application from './Application';
 require('./styles/base.scss');
